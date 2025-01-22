ae0631cb5449847c8d5cf8123c3208a4d4a22b50
Suwaathmi <suwaathmiravindran@gmail.com>
2025-01-22 23:07:02 +0530

    update

---
+++ VolunMate/VolunMate/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+
+namespace VolunMate
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Event handler for Home button click
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            ShowPanel(IntroPanel);
+        }
+
+        // Event handler for Login button click
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            ShowPanel(LoginPanel);
+        }
+
+        // Event handler for Sign Up button click
+        private void Signup_Click(object sender, RoutedEventArgs e)
+        {
+            ShowPanel(SignupPanel);
+        }
+
+        // Method to show the specified panel and hide others
+        private void ShowPanel(UIElement panel)
+        {
+            // Hide all panels
+            IntroPanel.Visibility = Visibility.Hidden;
+            LoginPanel.Visibility = Visibility.Hidden;
+            SignupPanel.Visibility = Visibility.Hidden;
+
+            // Show the specified panel
+            panel.Visibility = Visibility.Visible;
+        }
+    }
+}
